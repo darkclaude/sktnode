597f7ca486f6d1cb831b4c096b6da3c04ea918bf
Hamid <darkclaudevfx@gmail.com>
2016-04-11 14:59:53 +0000

    df

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.on('connection', function(){
+console.log('USer connected via Socket.io!');
+});
 app.use(express.static(__dirname + '/public'));
 http.listen(PORT, function(){
 console.log('Server started');
